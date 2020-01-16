8d6c469eff588fea59565a0c0ec2a747115e914a
Bhimsen Padalkar <bhim.padalkar@gmail.com>
2020-01-16 16:25:27 +0530

    Not generating duplicate params if same param has occured multiple times in the query

--- codegen/src/main/kotlin/norm/SqlAnalyzer.kt
+++ codegen/src/main/kotlin/norm/SqlAnalyzer.kt
@@ -19,7 +19,8 @@ data class ColumnModel(
 data class ParamModel(
     val name: String,
     val dbType: String,
-    val isNullable: Boolean
+    val isNullable: Boolean,
+    val places: List<Int>
 )
 
 data class SqlModel(
@@ -37,15 +38,22 @@ class SqlAnalyzer(private val connection: Connection) {
     fun sqlModel(namedParamSql: String): SqlModel {
 
         val paramNames = namedParamsRegex.findAll(namedParamSql).map { it.value }.toList()
+        val paramsIndexMapping = paramNames.foldIndexed(HashMap<String, List<Int>>(), { i, acc, param ->
+            acc[param] = acc[param]?.let { it + (i + 1) } ?: listOf(i + 1)
+            acc
+        })
+
         val preparableStatement = namedParamsRegex.replace(namedParamSql, "?")
         val preparedStatement = connection.prepareStatement(preparableStatement)
 
         val parameterMetaData = preparedStatement.parameterMetaData
-        val params = (1..parameterMetaData.parameterCount).map {
+
+        val params = paramsIndexMapping.map {
             ParamModel(
-                paramNames[it - 1].substring(1),
-                parameterMetaData.getParameterTypeName(it), // db type
-                parameterMetaData.isNullable(it) != ParameterMetaData.parameterNoNulls
+                it.key.substring(1),
+                parameterMetaData.getParameterTypeName(it.value[0]), // db type
+                parameterMetaData.isNullable(it.value[0]) != ParameterMetaData.parameterNoNulls,
+                it.value
             )
         }
 
@@ -61,7 +69,7 @@ class SqlAnalyzer(private val connection: Connection) {
                 )
             }
         } else { // it is a command
-            listOf<ColumnModel>()
+            listOf()
         }
         return SqlModel(params, res, preparableStatement)
     }
